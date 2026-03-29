59d87d588556b8ae01120758d317d347efc5f371
Robin van Baalen <robin@ganttify.com>
2026-03-29 08:30:24 -0400

    feat: port 7 library project docs to integrated project pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,16 +12,9 @@ export default defineConfig({
   redirects: {
     "/curacao-election-2025": "https://curacao-election-2025.robinvanbaalen.nl",
     "/debt-tracker": "https://debt-tracker.robinvanbaalen.nl",
-    "/domjs": "https://domjs.robinvanbaalen.nl",
-    "/eslint-config": "https://eslint-config.robinvanbaalen.nl",
     "/flow-invoice": "https://flow-invoice.robinvanbaalen.nl",
-    "/hashparser": "https://hashparser.robinvanbaalen.nl",
     "/json-beautify": "https://json-beautify.robinvanbaalen.nl",
-    "/readme-to-html": "https://readme-to-html.robinvanbaalen.nl",
     "/revenue-forecast": "https://revenue-forecast.robinvanbaalen.nl",
-    "/runner-manager": "https://runner-manager.robinvanbaalen.nl",
-    "/source-to-llm": "https://source-to-llm.robinvanbaalen.nl",
-    "/transitionjs": "https://transitionjs.robinvanbaalen.nl",
   },
 
   integrations: [
